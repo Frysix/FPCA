e0dea321c3582bd8a225dcd504ef838fe582a432
Frysix <0frysix0@gmail.com>
2025-06-08 02:44:33 -0400

    Installation Finder

--- Installer/Installer.ps1
+++ Installer/Installer.ps1
@@ -16,14 +16,17 @@ foreach ($Drive in $Drives) {
     $DriveLetters += $Drive.DeviceID
 }
 
+# Display the found drives
 Write-Host "Drives found:" -ForegroundColor Cyan
 $DriveLetters | ForEach-Object { Write-Host $_ }
 
-#Checks if there is an existing installation on any of the drives
+# Checks if there is an existing installation on any of the drives
+# If an installation is found, it reads the fpca.info file and displays the version and start file.
 foreach ($Letter in $DriveLetters) {
     if (test-path -path "$Letter\FPCA\fpca.info") {
         $info = @{}
         $section = ""
+        # Parse the fpca.info content
         foreach ($line in Get-Content "$Letter\FPCA\fpca.info") {
             $line = $line.Trim()
             if ($line -match "^\s*#|^\s*;|^\s*$") {
@@ -42,9 +45,39 @@ foreach ($Letter in $DriveLetters) {
         }
         Write-Host "Existing installation found on drive $Letter" -ForegroundColor Yellow
         Write-Host "Version: $($info['General']['Version'])" -ForegroundColor Yellow
-        
+        # Check the online version and ask for update if the online version is newer
+        $OnlineRawInfo = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Frysix/FPCA/refs/heads/main/Main/fpca.info" -UseBasicParsing
+        $lines = $OnlineRawInfo.Content -split "`n"
+        $OnlineInfo = @{}
+        $section = ""
+        # Parse the online fpca.info online content
+        foreach ($line in $lines) {
+            $line = $line.Trim()
+            if ($line -match "^\s*#|^\s*;|^\s*$") {
+                continue
+            }
+            if ($line -match "^\[(.+)\]$") {
+                $section = $matches[1]
+                $OnlineInfo[$section] = @{}
+            } elseif ($line -match "^(.*?)=(.*)$") {
+                $key = $matches[1].Trim()
+                $value = $matches[2].Trim()
+                if ($section -ne "") {
+                    $OnlineInfo[$section][$key] = $value
+                }
+            }
+        }
+        # Compare the online version with the local version and launch the installed script if the versions match
+        if ($OnlineInfo['General']['Version'] -eq $info['General']['Version']) {
+            if (test-path -path "$Letter\FPCA\$($info['Files']['start'])") {
+                Write-Host "Start file found: $($info['Files']['start'])" -ForegroundColor Yellow
+                Start-Process -FilePath "$Letter\FPCA\$($info['Files']['start'])" -WorkingDirectory "$Letter\FPCA" -Verb Runas
+                Exit
+            }
+        }
     }
 }
+
 
 
 #END REMOVE BEFORE PUBLICATION
--- Launcher/PSlauncher/obj/Debug/net8.0/PSlauncher.AssemblyInfo.cs
+++ Launcher/PSlauncher/obj/Debug/net8.0/PSlauncher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPCA Installer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+847451f9b906fdc0d6fd2ac1c81e956c5973d0b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f23c0ef540cad8a52e3f3da3c41339a5004b6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPCA Installer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPCA Installer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
