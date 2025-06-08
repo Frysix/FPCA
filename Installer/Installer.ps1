4350453b36b5b0ab8bcce2f350406d7768bb42fc
Frysix <0frysix0@gmail.com>
2025-06-08 00:49:25 -0400

    Test for changes

--- Installer/Installer.ps1
+++ Installer/Installer.ps1
@@ -1,3 +1,20 @@
-####TEST####
+# Standardized Installer Script for Windows
+
+# Starting Message
 Write-Host "Starting Installer script..."
-Pause
+
+# Checks if there is an existing installation
+
+# Ininitializes the $DriveLetters array
+$DriveLetters = @()
+# Get all logical disks with DriveType 2 (Removable) and 3 (Local Disk)
+$Drives = Get-CimInstance Win32_LogicalDisk | Where-Object {
+    $_.DriveType -in 2, 3
+}
+# Collects the DeviceID of each drive into the $DriveLetters array
+foreach ($Drive in $Drives) {
+    $DriveLetters += $Drive.DeviceID
+}
+
+Write-Host "Drives found:" -ForegroundColor Cyan
+$DriveLetters | ForEach-Object { Write-Host $_ }
